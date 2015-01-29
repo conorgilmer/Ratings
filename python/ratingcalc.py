7f8a2ee2a4dc6f244f6c621029ea0117b73c1512
Conor Gilmer <conor.gilmer@gmail.com>
2015-01-29 22:00:24 +0000

    python rating yearly calc

---
+++ python/ratingcalc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import csv
+
+totalgrades  = 0
+totalresults = 0.0
+rating       = 1650
+change       = 0
+extra        = 0
+
+dataFile = open('results.csv', "rb")
+reader = csv.reader(dataFile)
+
+print "\n *** Starting *** \n"
+print " Chess Rating Rating Calculator "
+print "Start Rating " + str(rating) + "\n"
+
+rownum       = 0 #row number
+for row in reader:
+  rdiff      = 0
+  extra      = 0
+  if rownum <> 0:
+    rdiff = int(row[2]) - rating
+    totalgrades = totalgrades + int(row[2]) 
+    totalresults = totalresults + float(row[3])
+    res = float(row[3])
+    opp = float(row[2])
+    if res == 0:
+      extra= -16
+      extra = extra + ((opp-rating)/25)
+    elif res == 0.5:
+      extra= 0
+      extra = extra + ((opp-rating)/25)
+    elif res == 1:
+      extra= 16
+      extra = extra + ((opp-rating)/25)
+      if extra < 0: 
+        extra=0
+    else:
+      print " Error No Match Result"
+    change = change + extra
+    print row[0] + "\t" + row[1] + "\t" + row[2] +"\t" +row[3]+ "\t" + str(rdiff) + "\t" + str(extra) 
+ 
+  else:
+    print row[0] + "\t" + row[1] + "\t" + row[2] +"\t" +row[3] + "\t Diff\t Change"
+  rownum = rownum + 1
+
+games = rownum - 1
+
+dataFile.close()
+
+print "\nAverage\t\t" + str(totalgrades/games) + "\t" + str(totalresults/games) +"\tChange " + str(change)
+print "games " + str(games)
+
+print "New Rating " + str(rating + change)
+
+print "\n *** The End *** \n"
